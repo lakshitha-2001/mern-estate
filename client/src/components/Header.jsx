c26ae1203b9a2525f7372a7e447a99f5b4369e0d
Lakshitha Madushan <harshanimaheshika999@gmail.com>
2025-02-03 07:34:35 +0530

    create and run server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,26 +8,28 @@ export default function Header() {
   // console.log('Avatar URL:', currentUser?.avatar);
 
   return (
-    <header className='bg-slate-300 shadow-md'>
+    <header className='bg-[#000431] shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         
+        <Link to='/'>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Madu</span>
-            <span className='text-slate-700'>Estate</span>
+            <span className='text-[#82CDCF]'>Madu</span>
+            <span className='text-[#F5FAFF]'>Estate</span>
         </h1>
+        </Link>
       
         <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
           <input type='text' placeholder='Search...'
             className='bg-transparent focus:outline-none w-24 sm:w-64' />
-          <FaSearch className='text-slate-600' />
+          <FaSearch className='text-[#000431]' />
         </form>
         
         <ul className='flex gap-4'>
           <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+            <li className='hidden sm:inline text-slate-50 hover:underline'>Home</li>
           </Link>
           <Link to='/about'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+            <li className='hidden sm:inline text-slate-50 hover:underline'>About</li>
           </Link>
           
           {/* <Link to='/profile'>
